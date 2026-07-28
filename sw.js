59373a3faa18ee9e0ebe6c9401be6a128f378f9e
sunmmerwindy <shengtanghua4@gmail.com>
2026-07-28 19:29:05 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const C='worklog-v2';
-self.addEventListener('install',e=>{self.skipWaiting()});
-self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim())});
-self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const rc=r.clone();caches.open(C).then(c=>c.put(e.request,rc));return r}).catch(()=>caches.match(e.request)))});
